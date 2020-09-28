4dd3df1166d72be6d0fc16a2b0e144bbae588e62
tcdat96 <tcdat96@gmail.com>
2020-09-28 18:34:58 -0500

    attempt to do statistical tests + add a scatter-plot for daily cases + refactor

--- hypothesis_8.py
+++ hypothesis_8.py
@@ -3,7 +3,7 @@
 
 # # Predefined functions
 
-# In[1]:
+# In[271]:
 
 
 import pandas as pd
@@ -17,7 +17,7 @@ def getData(url, sheet_name=0, skiprows=0, truncate=-1, index_column='County Nam
     return df
 
 
-# In[2]:
+# In[272]:
 
 
 def config_subplot(ax, label, fontsize=12, ticker_count=3):
@@ -34,14 +34,14 @@ def config_subplot(ax, label, fontsize=12, ticker_count=3):
 
 # ## Cumulative case count
 
-# In[3]:
+# In[273]:
 
 
 url = "https://dshs.texas.gov/coronavirus/TexasCOVID19DailyCountyCaseCountData.xlsx"
 cases_df = getData(url, skiprows=2, truncate=253)
 
 
-# In[4]:
+# In[274]:
 
 
 import re
@@ -54,12 +54,13 @@ dates = [datetime.strptime(date, '%m-%d') for date in cases_df.keys()]
 
 # ### Scale the data down to per 100k capita
 
-# In[5]:
+# In[275]:
 
 
 pop_url = "https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv"
 pop_df = pd.read_csv(pop_url, engine='python')
-pop_df = pop_df.set_index('CTYNAME')
+pop_df = pop_df.rename(columns={'CTYNAME': 'County Name'})
+pop_df = pop_df.set_index('County Name')
 # filter Texas counties
 pop_df = pop_df[pop_df['STNAME'] == 'Texas']
 # get latest population
@@ -68,7 +69,7 @@ pop_df = pop_df[pop_df.filter(like='2019').columns[0]]
 pop_df.index = pop_df.index.map(lambda x: x.replace(' County', ''))
 
 
-# In[44]:
+# In[276]:
 
 
 scaled_cases_df = cases_df.apply(lambda row: row * 1e5 / pop_df[row.index])
@@ -77,7 +78,7 @@ scaled_cases_df.shape
 
 # ## Metro / Non-metro
 
-# In[ ]:
+# In[277]:
 
 
 metro_url = "http://www.dshs.state.tx.us/chs/info/TxCoPhrMsa.xls"
@@ -86,17 +87,22 @@ metro_df = metro_df[[metro_df.filter(like='2013').columns[0], metro_df.filter(li
 #metro_df
 
 
-# In[223]:
+# In[278]:
 
 
-merged_df = pd.merge(scaled_cases_df, metro_df, on='County Name', how='outer')
-merged_df = merged_df.rename(columns={merged_df.keys()[-2]: 'Classification', merged_df.keys()[-1]: 'MetroArea'})
-#merged_df
+# population
+merged_df = pd.merge(pop_df, metro_df, on='County Name', how='inner')
+# metro classificiation
+merged_df = pd.merge(merged_df, scaled_cases_df, on='County Name', how='outer')
+# rename column
+keys = merged_df.keys()
+merged_df = merged_df.rename(columns={keys[0]: 'Population', keys[1]: 'Classification', keys[2]: 'MetroArea'})
+merged_df
 
 
 # # Plotting
 
-# In[33]:
+# In[279]:
 
 
 import matplotlib.pyplot as plt
@@ -111,7 +117,7 @@ ticker_count=8
 
 # ## 7-day average case count
 
-# In[105]:
+# In[280]:
 
 
 # create figure
@@ -127,7 +133,7 @@ config_subplot(ax2, 'Metro Area', title_size, ticker_count)
 # for label, df in merged_df.groupby('Classification', sort=True):
 metro_cls = ['Non-core', 'Micropolitan', 'Small Metro', 'Medium Metro', 'Large Fringe Metro', 'Large Central Metro']
 for cls in metro_cls:
-    df = merged_df[merged_df['Classification'] == cls].iloc[:,:-2]
+    df = merged_df[merged_df['Classification'] == cls].iloc[:,3:]
     df = df.mean().diff().rolling(window=7).mean()
     #print(df.iloc[:,:-1].max().max())
     ax1.plot(dates, df, label=cls)
@@ -135,7 +141,7 @@ for cls in metro_cls:
 # plotting average values of each metro classification
 metro_types = ['Non-Metro', 'Metro']
 for metro_type in metro_types:
-    df = merged_df[merged_df['MetroArea'] == metro_type].iloc[:,:-2]
+    df = merged_df[merged_df['MetroArea'] == metro_type].iloc[:,3:]
     df = df.mean().diff().rolling(window=7).mean()
     ax2.plot(dates, df, label=metro_type)
 
@@ -154,14 +160,28 @@ for line, name in zip(ax1.lines, metro_cls):
     
 fig.tight_layout()
 
+fig.savefig('graphs/hypothesis_8_1.png')
+
 
 # ## Heatmap
 
-# In[222]:
+# In[281]:
+
+
+import calendar
+
+avg_month_df = merged_df.iloc[:,:3]
+for month in range(1,12,1):
+    df = merged_df.filter(like='{:02d}-'.format(month))
+    if df.shape[1] > 0:
+        avg_month_df[calendar.month_name[month]] = df.mean(axis=1).values
+# avg_month_df
+
+
+# In[282]:
 
 
 import numpy as np
-import calendar
 from matplotlib.pyplot import cm
 
 fig_size = 6
@@ -170,23 +190,101 @@ fig2.suptitle('', fontsize=32)
 fig2.text(0.085, 0.5, "Cumulative case count (per 100,000 of population)", va="center", rotation=90, fontsize=14)
 
 idx = 0
-vmax = merged_df.iloc[:,:-2].max().max()
+vmax = merged_df.iloc[:,3:].max().max()
 for i, metro_type in enumerate(metro_types):
-    for month in range(1,12,1):
-        df = merged_df[merged_df['MetroArea'] == metro_type].iloc[:,:-2]
-        df = df.filter(like='{:02d}-'.format(month))
-        if df.shape[1] > 0:
-            df = df.mean(axis=1).to_numpy(dtype='float')
-            df = np.pad(df, (0,10-df.shape[0]%10), 'constant')
-            df = df.reshape((-1,10))
+    avg_month_type_df = avg_month_df[avg_month_df['MetroArea'] == metro_type].iloc[:,3:]
+    for month in avg_month_type_df:
+        col = avg_month_type_df[month].values
+        col = np.pad(col, (0, 10 - col.shape[0] % 10), 'constant')
+        col = col.reshape((-1,10))
             
-            idx += 1
-            ax = fig2.add_subplot(2, 8, idx)
-            ax.set_title(calendar.month_name[month], fontdict={'fontsize':12})
-            heatmap = ax.imshow(df, cmap=cm.Reds, vmax=vmax)
+        idx += 1
+        ax = fig2.add_subplot(2, 8, idx)
+        ax.set_title(month, fontdict={'fontsize':12})
+        heatmap = ax.imshow(col, cmap=cm.Reds, vmax=vmax)
     idx = 8
     
-fig2.colorbar(heatmap, orientation="horizontal")
-            
-fig2.savefig('Hypothesis_8_2.png')
+fig2.savefig('graphs/hypothesis_8_2.png')
+
+
+# ## Scatterplot
+
+# In[283]:
+
+
+import seaborn as sns
+
+cols=3
+fig3, axes = plt.subplots(2, cols, figsize=(15,10))
+fig3.suptitle('', fontsize=32)
+
+for month in range(4, 10, 1):
+    idx = month - 4
+    ax = ax=axes[int(idx/cols)][idx%cols]
+    g = sns.scatterplot(data=avg_month_df, x="Population", y=calendar.month_name[month], 
+                        hue='Classification', ax=ax)
+    g.set(xscale="log");
+    ax.get_legend().remove()
+#     plt.setp(ax.get_legend().get_texts(), fontsize='7')
+    
+handles, labels = axes[1][2].get_legend_handles_labels()
+fig3.legend(handles, labels, loc='upper right')
+
+fig3.savefig('graphs/hypothesis_8_3.png')
+
+
+# # Performing statistical tests
+
+# Null hypothesis (H0): There is no difference between spread rate of rural and urban counties.  
+# Alternative hypothesis (H1): Rural counties had less spread.
+
+# In[284]:
+
+
+from scipy import stats
+import time
+np.random.seed(int(time.time()))
+
+
+# # Paired T-Test
+
+# In[285]:
+
+
+case_metro_df = merged_df[merged_df['MetroArea'] == 'Metro'].iloc[:,3:]
+case_non_metro_df = merged_df[merged_df['MetroArea'] == 'Non-Metro'].iloc[:,3:]
+
+
+# In[286]:
+
+
+# stats.ttest_ind(case_non_metro_df.mean(), case_metro_df.mean(), equal_var = False)
+stats.ttest_rel(case_non_metro_df.mean(), case_metro_df.mean())
+
+
+# The p-value is 0.001, which is less than the stardard threshold 0.05 and 0.01, so we reject the null hypothesis and we can say that rural counties had less spread.
+
+# In[287]:
+
+
+#stats.chisquare(case_metro_df.mean(), case_non_metro_df.mean(), ddof=case_metro_df.shape[0]-1)
+#stats.chisquare(case_metro_df.iloc[:,30:].mean(), case_non_metro_df.iloc[:,30:].mean(), ddof=case_metro_df.shape[0]-1)
+
+
+# # ANOVA
+
+# In[288]:
+
+
+cls = merged_df['Classification']
+rural = merged_df[cls.isin(['Non-core', 'Micropolitan'])].iloc[:,3:]
+mixed = merged_df[cls.isin(['Small Metro', 'Medium Metro'])].iloc[:,3:]
+urban = merged_df[cls.isin(['Large Fringe Metro', 'Large Central Metro'])].iloc[:,3:]
+stats.f_oneway(rural.mean(), mixed.mean(), urban.mean())
+
+
+# In[289]:
+
+
+stats.f_oneway(*merged_df.groupby('Classification').mean().values)
 
